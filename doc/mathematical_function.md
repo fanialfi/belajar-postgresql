2b627491220bc4936ae199c417ba9f8bfb245f3e
fanialfi <fanialfirdaus@gmail.com>
2023-06-23 11:06:25 +0700

    mathemetical function

---
+++ doc/mathematical_function.md
@@ -0,0 +1,3 @@
+kumpulan function yang terdapat di PostgreSQL
+
+[sumber](https://www.postgresql.org/docs/15/functions-math.html)
---
+++ sql/mathematical_function.sql
@@ -0,0 +1,13 @@
+select pi();
+
+select abs(-999.9) as "absolute number";
+
+select factorial(10);
+
+select floor(31.99999999999999999994) as "pembulatan ke bawah";
+
+select trim_scale(31.99909910);
+
+select round(10.7) as "pembulatan", round(10.4) as "pembulatan 2";
+
+select round(random() * 10) as "angka random 1 - 10";
